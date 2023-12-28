f48f9b831f78c531fc013c7918bd01b415cc1a5f
Muhammad Amaz <amaz8144@gmail.com>
2023-12-28 19:32:26 +0500

    first commit

--- app/src/main/java/com/example/dating/Signup.java
+++ app/src/main/java/com/example/dating/Signup.java
@@ -2,7 +2,10 @@ package com.example.dating;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button phoneNum = findViewById(R.id.button3);
+
+        phoneNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PhoneNum.class);
+                startActivity(intent);
+            }
+        });
     }
 }
