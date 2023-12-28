691246890d66655ec21d5dd561c93bd6d657301c
Muhammad Amaz <amaz8144@gmail.com>
2023-12-28 19:18:17 +0500

    first commit!

--- app/src/main/java/com/example/dating/MainActivity.java
+++ app/src/main/java/com/example/dating/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.dating;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int age = 19;
+        TextView textView = findViewById(R.id.signIn);
+        Button createAcc = findViewById(R.id.button);
+
+        String text = "Already have an account? Sign In";
+
+        Spannable spannable = new SpannableString(text);
+        int startIndex = text.indexOf("Sign In");
+        spannable.setSpan(new ForegroundColorSpan(Color.parseColor("#E94057")), startIndex, startIndex + "Sign In".length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(spannable);
+
+        createAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
