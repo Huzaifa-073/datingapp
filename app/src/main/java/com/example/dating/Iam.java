08c874504f213b5df7fd016a5343dc46140b2b12
Muhammad Amaz <amaz8144@gmail.com>
2023-12-29 20:19:10 +0500

    commmit!

--- app/src/main/java/com/example/dating/Iam.java
+++ app/src/main/java/com/example/dating/Iam.java
@@ -1,8 +1,12 @@
 package com.example.dating;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Iam extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Iam extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iam);
+
+        CardView woman = findViewById(R.id.woman);
+        CardView man = findViewById(R.id.man);
+
+        woman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                woman.setBackgroundColor(Color.parseColor("#E94057"));
+                man.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+        man.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                woman.setBackgroundColor(Color.WHITE);
+                man.setBackgroundColor(Color.parseColor("#E94057"));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dating/Profile.java
+++ app/src/main/java/com/example/dating/Profile.java
@@ -11,6 +11,8 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Profile extends AppCompatActivity {
 
     @Override
@@ -22,17 +24,21 @@ public class Profile extends AppCompatActivity {
         Button confirmation = findViewById(R.id.button4);
         DatePicker datePicker = findViewById(R.id.datePicker);
         EditText firstName = findViewById(R.id.editText5);
-//        EditText LastName = findViewById(R.id.editText6);
+        TextInputLayout editText1 = findViewById(R.id.editTexts);
+        TextInputLayout editText2 = findViewById(R.id.editTexts1);
+        EditText LastName = findViewById(R.id.editText6);
         ImageView personImage = findViewById(R.id.imageView3);
 
         birthDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 datePicker.setVisibility(View.VISIBLE);
-                firstName.setVisibility(View.GONE);
-//                LastName.setVisibility(View.GONE);
-                birthDate.setVisibility(View.GONE);
-                personImage.setVisibility(View.GONE);
+                firstName.setVisibility(View.INVISIBLE);
+                LastName.setVisibility(View.INVISIBLE);
+                birthDate.setVisibility(View.INVISIBLE);
+                personImage.setVisibility(View.INVISIBLE);
+                editText1.setVisibility(View.INVISIBLE);
+                editText2.setVisibility(View.INVISIBLE);
             }
         });
 
