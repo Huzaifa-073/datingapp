a9d91cdbba18ecf2fd75720ab351376a23da805e
Muhammad Amaz <amaz8144@gmail.com>
2024-01-01 17:18:24 +0500

    commmit!

--- app/src/main/java/com/example/dating/Code.java
+++ app/src/main/java/com/example/dating/Code.java
@@ -1,6 +1,7 @@
 package com.example.dating;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +19,15 @@ public class Code extends AppCompatActivity {
 
         ImageView back = findViewById(R.id.back);
         TextView sendAgain = findViewById(R.id.sendagain);
+        ConstraintLayout constraintLayout = findViewById(R.id.constraint);
+
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
